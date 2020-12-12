6651aac7ec8da6588ecf7e91434a3d9662d99f8a
Krzysztof Sobkowiak <krzysztof_91@hotmail.com>
2020-12-12 22:54:05 +0100

    6.2 Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -8,17 +8,15 @@ public class TestingMain {
         int add = calculator.addAToB(7, 5);
         int substract = calculator.substractAFromB(15,5);
 
-        if(add == 12) {
+        if (add != 12) {
+            System.out.println("Error");
+        } else {
             System.out.println("Result calculator adding test: OK");
         }
-        else {
+        if (substract != 10) {
             System.out.println("Error");
-        }
-        if(substract == 10) {
+        } else {
             System.out.println("Result calculator substract test: OK");
-        }
-        else {
-            System.out.println("Error");
         }
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -1,10 +1,17 @@
 package com.kodilla.testing.forum;
 
 import com.kodilla.testing.user.SimpleUser;
-import  org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+@DisplayName("Forum The Suite")
 
 public class ForumTestSuite {
+    @DisplayName(
+            "When created SimpleUser with name, " +
+                    "then getUsername should return correct name"
+    )
+    
     @Test
     void TestCaseUserName() {
         //Given
