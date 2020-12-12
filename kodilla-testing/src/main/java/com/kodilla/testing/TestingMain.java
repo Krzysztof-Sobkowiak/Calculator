61a544178cb6485a813538aeb93c1fb322c7661a
Krzysztof Sobkowiak <krzysztof_91@hotmail.com>
2020-12-12 22:29:17 +0100

    6.2 Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,19 @@
 package com.kodilla.testing;
-import com.kodilla.testing.Calculator;
+import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
 
     public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
         Calculator calculator = new Calculator();
         int add = calculator.addAToB(7, 5);
         int substract = calculator.substractAFromB(15,5);
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.forum;
+
+import com.kodilla.testing.user.SimpleUser;
+import  org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ForumTestSuite {
+    @Test
+    void TestCaseUserName() {
+        //given
+        SimpleUser simpleUser = new SimpleUser()
+    }
+}
